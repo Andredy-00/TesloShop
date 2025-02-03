137f71bf7d78ee6352d77bc192c96d9cafbcb55f
Andredy Guerra <andredyguerra00@gmail.com>
2025-02-02 20:52:07 -0500

    Creando las rutas de la aplicacon

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -4,8 +4,8 @@ export default function ShopLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-red-600 min-h-screen">
+    <main className="bg-red-600 min-h-screen">
         {children}
-    </div>
+    </main>
   );
 }
