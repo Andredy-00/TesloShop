5600878b1c7b9b031a01714ad1b0ecf4ae24c6a9
andredy-00 <andredyguerra00@gmail.com>
2025-02-13 22:15:03 -0500

    Pantalla de categoria

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,23 +1,43 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/components/interfaces";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+
+const seedProducts = initialData.products;
 
 interface Props {
   params: {
-      id:string;
+      id: Category;
   }
 }
 
 export default function( { params }: Props ) {
 
   const {id} = params;
+  const products = seedProducts.filter( product => product.gender === id);
 
-  if (id === 'kids'){
-    notFound();
+  const labels: Record<Category, string> = {
+    'men': 'Hombres',
+    'women': 'Mujeres',
+    'kid': 'Niños',
+    'unisex': 'Unisex'
   }
 
+ /*  if (id === 'kids'){
+    notFound();
+  } */
+
     return (
-     <div>
-      <h1>category Page {id}</h1>
-     </div>
+      <>
+         <Title
+           title={`Articulos para ${labels[id]}`}
+           subtiitle="Todos los productos"
+           className="mb-2"
+         />
+     
+         <ProductGrid products={products} />
+     
+        </>
     );
   }
   
