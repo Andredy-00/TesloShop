09932b193797394542ac7bf4d0a31fdfd45c6264
andredy-00 <andredyguerra00@gmail.com>
2025-02-14 13:36:50 -0500

    Slideshow con Thumbs | Fase dos

--- src/components/product/slideshow/ProductSlideshow.tsx
+++ src/components/product/slideshow/ProductSlideshow.tsx
@@ -11,7 +11,7 @@ import "swiper/css/navigation";
 import "swiper/css/thumbs";
 
 import "./slidershow.css";
-import { FreeMode, Navigation, Thumbs } from "swiper/modules";
+import { Autoplay, FreeMode, Navigation, Thumbs } from "swiper/modules";
 import Image from "next/image";
 
 interface Props {
@@ -33,8 +33,12 @@ export const ProductSlideshow = ({ images, title, className }: Props) => {
         }
         spaceBetween={10}
         navigation={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
         thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Navigation, Thumbs]}
+        modules={[FreeMode, Navigation, Thumbs, Autoplay]}
         className="mySwiper2"
       >
         {images.map((image) => (
@@ -48,6 +52,29 @@ export const ProductSlideshow = ({ images, title, className }: Props) => {
             />
           </SwiperSlide>
         ))}
+      </Swiper>
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        spaceBetween={10}
+        slidesPerView={4}
+        freeMode={true}
+        watchSlidesProgress={true}
+        modules={[FreeMode, Navigation, Thumbs]}
+        className="mySwiper"
+      >
+        {
+            images.map((image) => (
+                <SwiperSlide key={image}>
+                    <Image
+                        width={300}
+                        height={300}
+                        src={`/products/${image}`}
+                        alt={title}
+                        className="rounded-lg object-fill"
+                    />
+                </SwiperSlide>
+            ))
+        }
       </Swiper>
     </div>
   );
