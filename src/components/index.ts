ded822ff06efd70eb77d2632ddfbadd399bf9dd6
andredy-00 <andredyguerra00@gmail.com>
2025-02-13 23:15:12 -0500

    Selector de tallas

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,8 @@
 export * from './products/product-grid/ProductGrid';
 export * from './products/product-grid/ProductGridIteam';
 
+export * from './product/size-selector/SizeSelector';
+
 export * from './ui/top-menu/TopMenu';
 export * from './ui/not-found/PageNotFound';
 export * from './ui/title/Title';
